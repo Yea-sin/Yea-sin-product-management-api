e09047c192647387f7f30d143552d4515bd6d965
Yeasin Arafat <yeasinn2002@gmail.com>
2024-05-24 23:09:51 +0600

    product validate schema updated

--- src/app/modules/product/product.validate.ts
+++ src/app/modules/product/product.validate.ts
@@ -1,23 +1,25 @@
 import { z } from "zod";
 
 export const variantValidateSchema = z.object({
-  type: z.string({ message: "Product variant type is required!" }),
-  value: z.string({ message: "Product variant value is required!" }),
+  type: z.string({ message: "Product variant type must be string!" }),
+  value: z.string({ message: "Product variant value must be string!" }),
 });
 
 export const inventoryValidateSchema = z.object({
-  quantity: z.number({ message: "Product quantity is required!" }),
+  quantity: z.number({ message: "Product quantity must be number!" }),
   inStock: z.boolean({
-    message: "Product stock availability is required!",
+    message: "Product stock must be boolean!",
   }),
 });
 
 export const productValidateSchema = z
   .object({
-    name: z.string({ message: "Product name is required!" }),
+    name: z.string({ message: "Product name must be string!" }),
     description: z.string(),
-    price: z.number({ message: "Product price is required!" }),
-    category: z.string().nonempty({ message: "Product Category is required!" }),
+    price: z.number({ message: "Product price must be number!" }),
+    category: z
+      .string()
+      .nonempty({ message: "Product Category must be string!" }),
     tags: z.array(z.string()).nonempty({
       message: "Tags cannot be empty!",
     }),
